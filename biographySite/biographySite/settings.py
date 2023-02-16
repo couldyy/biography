d38217484a54ce251eeb592364a8084de1984479
couldyy <couldy111@gmail.com>
2023-02-16 09:36:01 +0200

    second commit

--- biographySite/biographySite/settings.py
+++ biographySite/biographySite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p12y%k!a%b*sp#qo7#gt_zreg*&__(%s6+7vi692pjhr1t5+e='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.dmitrypinchuk.fun']
 
 
 # Application definition
